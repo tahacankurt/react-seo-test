79f1b7e86c9b7f87d8e5364eb96b4aae5705d437
Taha Cankurt <Cankurt.Taha@test.local>
2019-02-18 13:51:40 +0300

    First Commit

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Layout from '../components/Layout'
+import Head from 'next/head'
+
+const About = () => (
+    <div>
+        <Layout>
+            <Head>
+                <title>About Page</title>
+                <meta name="description" content="About Page Description"/>
+                <meta name="keywords" content="About,page,keywords"/>
+            </Head>
+            <h1>About Next.js</h1>
+        </Layout>        
+    </div>
+)
+export default About;
